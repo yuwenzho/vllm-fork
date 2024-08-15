88a906364c8c0f4e69d803bf046ce7b28041cb05
yuwenzho <yuwen.zhou@intel.com>
2024-08-15 09:05:29 +0800

    Update benchmark_throughput.py

--- benchmarks/benchmark_throughput.py
+++ benchmarks/benchmark_throughput.py
@@ -16,9 +16,6 @@ from vllm.engine.arg_utils import EngineArgs
 from vllm.model_executor.layers.quantization import QUANTIZATION_METHODS
 from vllm.utils import FlexibleArgumentParser
 
-old = torch._C._dispatch_tls_is_dispatch_key_excluded(
-    torch._C.DispatchKey.ADInplaceOrView
-)
 torch._C._dispatch_tls_set_dispatch_key_excluded(
     torch._C.DispatchKey.ADInplaceOrView, True
 )
@@ -93,7 +90,6 @@ def run_vllm(
     download_dir: Optional[str] = None,
     load_format: str = EngineArgs.load_format,
 ) -> float:
-    # from vllm import LLM, SamplingParams
     from vllm.entrypoints.llm import LLM
     from vllm.sampling_params import SamplingParams
     llm = LLM(
@@ -143,19 +139,6 @@ def run_vllm(
     llm.generate(prompts, sampling_params, use_tqdm=True)
     end = time.perf_counter()
     return end - start
-
-    # start = time.perf_counter()
-    # for warmup_idx in range(1):
-    #     print("="*50, "warm up", warmup_idx + 1, "="*50)
-    #     llm.generate(prompts, sampling_params, use_tqdm=True)
-    #     exit(0)
-    # end = time.perf_counter()
-
-    # print("="*50, "after warmup", "="*50)
-    # start = time.perf_counter()
-    # outputs = llm.generate(prompts, sampling_params, use_tqdm=True)
-    # end = time.perf_counter()
-    # return end - start
 
 def run_hf(
     requests: List[Tuple[str, int, int]],
